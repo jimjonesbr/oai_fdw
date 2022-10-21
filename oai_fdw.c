e9c7d844142cc0e13f2eaa1e76c91c88e6f37e64
Jim Jones <jim.jones@uni-muenster.de>
2022-10-21 08:09:05 +0200

    set version to 1.3.0 (forgot in last release)

--- oai_fdw.c
+++ oai_fdw.c
@@ -66,7 +66,7 @@
 #include "access/reloptions.h"
 #include "catalog/pg_namespace.h"
 
-#define OAI_FDW_VERSION "1.2.0"
+#define OAI_FDW_VERSION "1.3.0"
 #define OAI_REQUEST_LISTRECORDS "ListRecords"
 #define OAI_REQUEST_LISTIDENTIFIERS "ListIdentifiers"
 #define OAI_REQUEST_IDENTIFY "Identify"
