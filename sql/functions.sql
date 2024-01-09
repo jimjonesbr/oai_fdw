aba5b0ab8ffd7aadecc676a0e5fcc3b052b5f7f7
Jim Jones <jim.jones@uni-muenster.de>
2024-01-09 09:53:41 +0100

    Add COLLATE C to ORDER BY in regression tests

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,37 +1,37 @@
 SELECT OAI_Version() IS NULL;
 SELECT * FROM OAI_ListMetadataFormats('oai_server_ulb') 
-ORDER BY schema;
+ORDER BY metadataprefix COLLATE "C";
 
 SELECT * FROM OAI_ListMetadataFormats('oai_server_dnb') 
-ORDER BY schema;
+ORDER BY metadataprefix COLLATE "C";
 
 SELECT * FROM OAI_ListMetadataFormats('oai_server_bioj') 
-ORDER BY schema;
+ORDER BY metadataprefix COLLATE "C";
 
 SELECT * FROM OAI_ListMetadataFormats('oai_server_bjihs') 
-ORDER BY schema;
+ORDER BY metadataprefix COLLATE "C";
 
 SELECT * FROM OAI_ListMetadataFormats('oai_server_davidrumsey') 
-ORDER BY schema;
+ORDER BY metadataprefix COLLATE "C";
 
 SELECT * FROM OAI_ListSets('oai_server_ulb') 
 WHERE setname IS NOT NULL AND setspec IS NOT NULL 
-ORDER BY setname 
+ORDER BY setname  COLLATE "C"
 FETCH FIRST ROW ONLY;
 
 SELECT * FROM OAI_ListSets('oai_server_dnb') 
 WHERE setname IS NOT NULL AND setspec IS NOT NULL 
-ORDER BY setname 
+ORDER BY setname  COLLATE "C"
 FETCH FIRST ROW ONLY;
 
 SELECT * FROM OAI_ListSets('oai_server_bjihs') 
 WHERE setname IS NOT NULL AND setspec IS NOT NULL 
-ORDER BY setname 
+ORDER BY setname  COLLATE "C"
 FETCH FIRST ROW ONLY;
 
 SELECT * FROM OAI_ListSets('oai_server_davidrumsey') 
 WHERE setname IS NOT NULL AND setspec IS NOT NULL 
-ORDER BY setname 
+ORDER BY setname  COLLATE "C"
 FETCH FIRST ROW ONLY;
 
 SELECT count(*) > 0 
@@ -39,20 +39,20 @@ FROM OAI_ListSets('oai_server_dnb')
 WHERE setname IS NOT NULL AND setspec IS NOT NULL;
 
 SELECT * FROM OAI_ListSets('oai_server_bioj')
-ORDER BY setspec;
+ORDER BY setspec COLLATE "C";
 
 SELECT * FROM OAI_Identify('oai_server_ulb')
-ORDER BY name;
+ORDER BY name COLLATE "C";
 
 SELECT * FROM OAI_Identify('oai_server_dnb')
-ORDER BY name;
+ORDER BY name COLLATE "C";
 
 SELECT * FROM OAI_Identify('oai_server_bioj')
-ORDER BY name;
+ORDER BY name COLLATE "C";
 
 SELECT * FROM OAI_Identify('oai_server_bjihs')
-ORDER BY name;
+ORDER BY name COLLATE "C";
 
 SELECT * FROM OAI_Identify('oai_server_davidrumsey')
-ORDER BY name;
+ORDER BY name COLLATE "C";
 
