47dde75125eb6ebfd334513cb378a5012c71b852
Jim Jones <jim.jones@uni-muenster.de>
2024-01-09 09:28:44 +0100

    Minor change to function regression tests (ORDER BY)

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,10 +1,18 @@
 SELECT OAI_Version() IS NULL;
-SELECT * FROM OAI_ListMetadataFormats('oai_server_ulb') ORDER BY metadataprefix;
-SELECT * FROM OAI_ListMetadataFormats('oai_server_dnb') ORDER BY metadataprefix;
-SELECT * FROM OAI_ListMetadataFormats('oai_server_bioj') ORDER BY metadataprefix;
-SELECT * FROM OAI_ListMetadataFormats('oai_server_bjihs') ORDER BY metadataprefix;
---SELECT * FROM OAI_ListMetadataFormats('oai_server_kbnl');
-SELECT * FROM OAI_ListMetadataFormats('oai_server_davidrumsey');
+SELECT * FROM OAI_ListMetadataFormats('oai_server_ulb') 
+ORDER BY metadataprefix;
+
+SELECT * FROM OAI_ListMetadataFormats('oai_server_dnb') 
+ORDER BY metadataprefix;
+
+SELECT * FROM OAI_ListMetadataFormats('oai_server_bioj') 
+ORDER BY metadataprefix;
+
+SELECT * FROM OAI_ListMetadataFormats('oai_server_bjihs') 
+ORDER BY metadataprefix;
+
+SELECT * FROM OAI_ListMetadataFormats('oai_server_davidrumsey') 
+ORDER BY metadataprefix;
 
 SELECT * FROM OAI_ListSets('oai_server_ulb') 
 WHERE setname IS NOT NULL AND setspec IS NOT NULL 
@@ -21,11 +29,6 @@ WHERE setname IS NOT NULL AND setspec IS NOT NULL
 ORDER BY setname 
 FETCH FIRST ROW ONLY;
 
---SELECT * FROM OAI_ListSets('oai_server_kbnl')
---WHERE setname IS NOT NULL AND setspec IS NOT NULL
---ORDER BY setname
---FETCH FIRST ROW ONLY;
-
 SELECT * FROM OAI_ListSets('oai_server_davidrumsey') 
 WHERE setname IS NOT NULL AND setspec IS NOT NULL 
 ORDER BY setname 
@@ -35,12 +38,21 @@ SELECT count(*) > 0
 FROM OAI_ListSets('oai_server_dnb') 
 WHERE setname IS NOT NULL AND setspec IS NOT NULL;
 
--- must fail: repositoriy no longer exists
-SELECT * FROM OAI_ListSets('oai_server_bioj');
-SELECT * FROM OAI_Identify('oai_server_ulb');
-SELECT * FROM OAI_Identify('oai_server_dnb');
-SELECT * FROM OAI_Identify('oai_server_bioj');
-SELECT * FROM OAI_Identify('oai_server_bjihs');
---SELECT * FROM OAI_Identify('oai_server_kbnl');
-SELECT * FROM OAI_Identify('oai_server_davidrumsey');
+SELECT * FROM OAI_ListSets('oai_server_bioj')
+ORDER BY 1;
+
+SELECT * FROM OAI_Identify('oai_server_ulb')
+ORDER BY 1;
+
+SELECT * FROM OAI_Identify('oai_server_dnb')
+ORDER BY 1;
+
+SELECT * FROM OAI_Identify('oai_server_bioj')
+ORDER BY 1;
+
+SELECT * FROM OAI_Identify('oai_server_bjihs')
+ORDER BY 1;
+
+SELECT * FROM OAI_Identify('oai_server_davidrumsey')
+ORDER BY 1;
 
