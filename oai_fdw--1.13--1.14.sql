6894158c3ac9c5af75f80f3dc0b453ef4ee5b47b
Jim Jones <jim.jones@uni-muenster.de>
2026-08-03 06:10:34 +0200

    Fix functions signatures
    Changing the OAI functions from IMMUTABLE to VOLATILE.

--- oai_fdw--1.13--1.14.sql
+++ oai_fdw--1.13--1.14.sql
@@ -1,1 +1,21 @@
-\echo "No change in the SQL instructions."
+CREATE OR REPLACE FUNCTION OAI_Version()
+  RETURNS text AS 'MODULE_PATHNAME', 'oai_fdw_version'
+  LANGUAGE C VOLATILE STRICT;
+  
+  COMMENT ON FUNCTION OAI_Version() IS 'Shows current version of oai_fdw and its major libraries';
+
+CREATE OR REPLACE FUNCTION OAI_ListMetadataFormats(text)
+  RETURNS SETOF OAI_MetadataFormat AS 'MODULE_PATHNAME', 'oai_fdw_listMetadataFormats'
+  LANGUAGE C VOLATILE STRICT;
+  
+  COMMENT ON FUNCTION OAI_ListMetadataFormats(text) IS 'Lists all metadata formats of a given OAI-PMH server';
+
+CREATE OR REPLACE FUNCTION OAI_ListSets(text)
+  RETURNS SETOF OAI_Set AS 'MODULE_PATHNAME', 'oai_fdw_listSets'
+  LANGUAGE C VOLATILE STRICT;
+  
+  COMMENT ON FUNCTION OAI_ListSets(text) IS 'Lists all sets of a given OAI-PMH server';
+  
+CREATE OR REPLACE FUNCTION OAI_Identify(text)
+  RETURNS SETOF OAI_IdentityNode AS 'MODULE_PATHNAME', 'oai_fdw_identity'
+  LANGUAGE C VOLATILE STRICT;
--- oai_fdw--1.14.sql
+++ oai_fdw--1.14.sql
@@ -25,25 +25,25 @@ CREATE TYPE OAI_MetadataFormat AS (
 
 CREATE FUNCTION OAI_Version()
   RETURNS text AS 'MODULE_PATHNAME', 'oai_fdw_version'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+  LANGUAGE C VOLATILE STRICT;
   
   COMMENT ON FUNCTION OAI_Version() IS 'Shows current version of oai_fdw and its major libraries';
 
 CREATE FUNCTION OAI_ListMetadataFormats(text)
   RETURNS SETOF OAI_MetadataFormat AS 'MODULE_PATHNAME', 'oai_fdw_listMetadataFormats'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+  LANGUAGE C VOLATILE STRICT;
   
   COMMENT ON FUNCTION OAI_ListMetadataFormats(text) IS 'Lists all metadata formats of a given OAI-PMH server';
 
 CREATE FUNCTION OAI_ListSets(text)
   RETURNS SETOF OAI_Set AS 'MODULE_PATHNAME', 'oai_fdw_listSets'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+  LANGUAGE C VOLATILE STRICT;
   
   COMMENT ON FUNCTION OAI_ListSets(text) IS 'Lists all sets of a given OAI-PMH server';
   
 CREATE FUNCTION OAI_Identify(text)
   RETURNS SETOF OAI_IdentityNode AS 'MODULE_PATHNAME', 'oai_fdw_identity'
-  LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+  LANGUAGE C VOLATILE STRICT;
   
   COMMENT ON FUNCTION OAI_Identify(text) IS 'Lists settings of a given OAI-PMH server';
   
